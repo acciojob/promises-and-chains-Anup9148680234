5866f1bd2e9fb0ff702d9988c21723af918cb55b
Anup Angadi <52240005+Anup9148680234@users.noreply.github.com>
2023-04-14 13:23:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ btn.addEventListener('click', function() {
 		return new Promise((resolve,reject)=>{
 			let a = Name.value;
 			let b = Age.value;
-			if(a.trim()=="")
-			
+			if(a.trim()=="" || b.trim()==""){
+				alert("Please enter valid details")
+			}
 			else if(b>18){
 				setTimeOut(()=> resolve(`Welcome, ${a}. You can vote`),4000);
 			}
